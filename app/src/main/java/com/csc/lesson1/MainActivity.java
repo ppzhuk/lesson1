6f025c28c1fcf0ff301e91fe766c7fc4f6099cb0
ppzhuk <ppzhuk@gmail.com>
2016-02-16 16:46:51 +0300

    add image downloading

--- app/src/main/java/com/csc/lesson1/MainActivity.java
+++ app/src/main/java/com/csc/lesson1/MainActivity.java
@@ -2,6 +2,9 @@ package com.csc.lesson1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +13,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // TODO: download photo
-        // TODO: add save orientation switches
+        String url =
+                "https://compscicenter.ru/media/cache/04/af/04af00c38359378a149700f9f9786f27.jpg";
+        ImageView photo = (ImageView)findViewById(R.id.photo);
+
+        // Picasso library atomatically caches loaded image.
+        // So image isn't downloaded again after orientational changes, but used from cache.
+        Picasso.with(this)
+                .load(url)
+                .into(photo);
     }
 }
